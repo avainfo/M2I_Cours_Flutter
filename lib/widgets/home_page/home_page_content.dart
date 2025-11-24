4bdf0d6202fe0d2e5c1b304916c147db2428a47c
Antonin Do Souto <avainfoconseils@gmail.com>
2025-11-24 10:00:41 +0100

    refactor: Remove FutureBuilder and HTTP logic from HomePageContent
    This commit refactors the `HomePageContent` widget by removing all data-fetching logic. The `FutureBuilder`, which was previously used to make an HTTP request to fetch and display a list of todos, has been removed.
    
    Changes:
    - Delete the `launchRequest` method responsible for fetching data from the JSONPlaceholder API.
    - Remove the `FutureBuilder` and its child `ListView.builder` from the `build` method.
    - Remove unused imports for `dart:convert`, `http`, and the `Todo` model.
    - The `HomePageContent` widget now returns an empty `Container`.

--- lib/widgets/home_page/home_page_content.dart
+++ lib/widgets/home_page/home_page_content.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:m2i_cours_flutter/data/models/todo.dart';
 
 class HomePageContent extends StatelessWidget {
   const HomePageContent({
@@ -11,47 +7,6 @@ class HomePageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height / 1.5,
-      width: MediaQuery.of(context).size.width / 1.25,
-      color: Colors.pink,
-      child: Center(
-        child: FutureBuilder<List<Todo>>(
-          future: launchRequest(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            }
-            if (snapshot.hasError) return Text("Error: ${snapshot.data}");
-            return ListView.builder(
-              itemBuilder: (context, index) => Row(
-                children: [
-                  Text("Id(${snapshot.data![index].id}): ${snapshot.data![index].title}"),
-                  SizedBox(width: 30,),
-                  Checkbox(
-                    value: snapshot.data![index].completed,
-                    onChanged: (value) => {},
-                  ),
-                ],
-              ),
-              itemCount: snapshot.data?.length,
-            );
-          },
-        ),
-      ),
-    );
-  }
-
-  Future<List<Todo>> launchRequest() async {
-    var response = await http.get(
-      Uri.parse("https://jsonplaceholder.typicode.com/todos"),
-    );
-    print(response.body);
-    List<Todo> todos = [];
-
-    for(var todo in jsonDecode(response.body)) {
-      todos.add(Todo.fromJson(todo));
-    }
-    return todos;
+    return Container();
   }
 }
